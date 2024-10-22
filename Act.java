045ef904d11e6ef77e933cbc4ddb61f8f2b04d41
sachin <sachinsy165@gmail.com>
2024-10-22 15:23:20 +0530

    file have been added

---
+++ Act.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Act 
+{
+
+	public static void name() 
+	{
+		try {
+			int x=100/0;
+		}
+		 
+		catch (Exception e) 
+		{
+			 System.out.println("Handled");
+		}
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		name();
+	}
+}
